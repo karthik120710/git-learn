fae5413d164e2a60a0d72a70420df6e459b198b0
karthik rajan <karthikravi2630@gmail.com>
2024-02-04 16:35:30 +0530

    Positive number code Done

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-       
+       positiveNo(5);
+    }
+    public static void positiveNo(int n){
+        if(n>0)
+            System.out.println(n+" is Positive number");
     }
 
    
